407b16213aacd83e8969056373529564705ae8db
brainsandwich <alexandrebeaudet@protonmail.com>
2016-09-18 23:49:47 +0200

    Twiddling with sketch

--- test/sketch/main.cpp
+++ test/sketch/main.cpp
@@ -72,30 +72,56 @@ int main(int argc, char const *argv[]) {
 	uss::modular::Enveloppe enveloppe(&context);
 	enveloppe.attack.value = 0.01f;
 	enveloppe.decay.value = 0.01f;
-	enveloppe.release.value = 0.01f;
+	enveloppe.release.value = 0.5f;
 	enveloppe.attackLevel.value = 1.0f;
 	enveloppe.sustainLevel.value = 1.0f;
 
-	uss::modular::Oscillator trigger(&context);
-	trigger.wavetable = &uss::core::squaretable;
-	trigger.unipolar = true;
-	trigger.frequency.value = 20.0f;
-	trigger.destination.connect(&enveloppe.gate);
+	uss::modular::Clock clk(&context);
+	clk.frequency.value = uss::core::bpmtf(120.0 * 5.0f);
+
+	uss::modular::Sequencer sequencer(&context);
+	clk.destination.connect(&sequencer.clock);
+	sequencer.noteLength.value = 0.005f;
+	sequencer.gate.connect(&enveloppe.gate);
+	sequencer
+		.addNote(60.0f)
+		.addNote(60.0f)
+		.addNote(60.0f)
+		.addNote(240.0f)
+		.addNote(60.0f)
+		.addNote(60.0f)
+		.addNote(160.0f)
+		.addNote(130.0f);
+
+	uss::modular::Oscillator lfo(&context);
+	lfo.frequency.value = 0.1f;
+	lfo.unipolar = true;
+
+	uss::modular::Gain lfogain(&context);
+	lfogain.gain.value = 0.1f;
+	lfogain.offset.value = 0.01f;
+	lfo.destination.connect(&lfogain.input);
+	lfogain.destination.connect(&sequencer.noteLength);
 
 	uss::modular::Oscillator osc(&context);
-	osc.frequency.value = 500.0f;
+	sequencer.currentNote.connect(&osc.frequency);
 	osc.destination.connect(&enveloppe.input);
+	osc.wavetable = &uss::core::squaretable;
 
 	uss::modular::Mixer mixer(&context);
-	enveloppe.destination.connect(&mixer.getMonoInput(0));
 	mixer.destination.connect(&device.output);
+
+	uss::modular::LowPassFilter lpf(&context);
+	lpf.quality = 4.7f;
+	lpf.cutoff = 500.0f;
+	lpf.destination.connect(&mixer.getMonoInput(0));
+	enveloppe.destination.connect(&lpf.input);
 
 	std::size_t count = 0;
 	context.start(44100, 64);
 	while (true) {
-		std::this_thread::sleep_for(10ms);
-		std::cout << "env: " << enveloppe.enveloppe.value << std::endl;
-		if (count++ >= 3000)
+		std::this_thread::sleep_for(1ms);
+		if (count++ >= 30000)
 			break;
 	}
 
